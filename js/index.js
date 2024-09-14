ef93efc608074f451929468fa6cc49e02e3507d6
nematullayev <nematullayevdilyorbek4@gmail.com>
2024-09-14 21:38:40 +0300

    full

--- js/index.js
+++ js/index.js
@@ -3,8 +3,55 @@ function getElement(selct, selector = document) {
 }
 
 const token = localStorage.getItem("token");
-console.log(token);
 
-if (!token) {
-  window.location.replace(`../login.html`);
-}
+const newTem = getElement(".template");
+const elWrapper = getElement(".hero");
+const left = getElement("#left");
+const elImg = getElement("#img");
+const elTitle = getElement("#title");
+const elDescription = getElement("#description");
+const elBtn = getElement("#btn");
+const btn = getElement("#rem");
+console.log(img);
+left.addEventListener("click", () => {
+  console.log("sal");
+  localStorage.removeItem("token");
+  if (!token) {
+    window.location.replace(`../login.html`);
+  }
+});
+btn.addEventListener("click", () => {
+  elImg.textContent = "";
+  elDescription.textContent = "";
+  elTitle.textContent = "";
+});
+
+const products = [];
+elBtn.addEventListener("click", () => {
+  const product = {
+    title: elTitle.value,
+    img: elImg.value,
+    description: elDescription.value,
+  };
+
+  products.push(product);
+
+  function createCards(items) {
+    elWrapper.textContent = null;
+
+    let createCards = items.map((item) => {
+      const newEl = newTem.content.cloneNode(true);
+
+      const topImg = getElement(".card-img-top", newEl);
+      const topTitle = getElement(".card-title", newEl);
+      const topDes = getElement(".card-text", newEl);
+
+      topImg.src = item.img;
+      topTitle.textContent = item.title;
+      topDes.textContent = item.description;
+
+      elWrapper.appendChild(newEl);
+    });
+  }
+  createCards(products);
+});
